7086a474dbb08b04596fb64360d4473bd6a05e95
yyao39 <yyao39@wisc.edu>
2025-09-21 14:05:01 -0700

    deps

--- generate-transcription/main.py
+++ generate-transcription/main.py
@@ -1,6 +1,18 @@
 import functions_framework
 import logging
 
+# Imports the Cloud Logging client library
+import google.cloud.logging
+
+# Instantiates a client
+client = google.cloud.logging.Client()
+
+# Retrieves a Cloud Logging handler based on the environment
+# you're running in and integrates the handler with the
+# Python logging module. By default this captures all logs
+# at INFO level and higher
+client.setup_logging()
+
 
 @functions_framework.http
 def generate_transcript(request):
